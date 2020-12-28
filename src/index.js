f12d9e07947b6ad6477f7dc29c71925a8f76c1d8
elaristocrata <victor.mendoza@tecno.com.mx>
2020-12-28 00:58:47 -0600

    conf de redux para mostrar info de usuario en el global STATE

--- src/index.js
+++ src/index.js
@@ -3,18 +3,44 @@ import ReactDOM from 'react-dom';
 import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
 import 'semantic-ui-css/semantic.min.css'
-import { BrowserRouter as Router, Switch, Route} from "react-router-dom";
+import {BrowserRouter as Router, Switch, Route, withRouter} from "react-router-dom";
 import Login from "./Auth/login";
 import Register from "./Auth/register";
+import firebase from "./firebase/firebase";
+import {createStore} from "redux";
+import {Provider, connect} from 'react-redux'
+import {composeWithDevTools} from "redux-devtools-extension";
+import rootReducer from "./reducers";
+import {setUser} from "./actions";
 
-const Root = () =>(
+const store =createStore(rootReducer, composeWithDevTools());
+class Root extends React.Component {
+    componentDidMount() {
+        firebase.auth().onAuthStateChanged(user => {
+            if (user) {
+                this.props.setUser(user);
+                this.props.history.push('/');
+            }
+        })
+    }
+
+    render() {
+        return (
+            <Switch>
+                <Route exact path="/" component={App}/>
+                <Route path="/login" component={Login}/>
+                <Route path="/register" component={Register}/>
+            </Switch>
+
+        );
+    }
+}
+
+const RootWhitAuth = withRouter(connect(null,{setUser})(Root));
+ReactDOM.render(
+    <Provider store={store}>
     <Router>
-        <Switch>
-            <Route exact path="/" component={App} />
-            <Route path="/login" component={Login}/>
-            <Route path="/register" component={Register}/>
-        </Switch>
+        <RootWhitAuth/>
     </Router>
-)
-ReactDOM.render(<Root />, document.getElementById('root'));
+    </Provider>, document.getElementById('root'));
 registerServiceWorker();
